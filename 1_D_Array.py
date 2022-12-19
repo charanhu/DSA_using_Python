cfe9d0a16e0fd95a36816819b8ec1a576148cb7f
charanhu <charanhumail@gmail.com>
2022-12-19 20:31:35 +0530

    one-D array

---
+++ 1_D_Array.py
@@ -0,0 +1,21 @@
+# print statement to ask the array size
+print("How many elements do you want to store?:", end="")
+# to store the array size
+size  = int(input())
+# declare an array
+array = []
+# ask the user to enter the array elements
+print("\nEnter", size, "elements:\n")
+# loop to store the array elements
+for i in range(size):
+    # to store the array element
+    element = int(input())
+    # append the element to the array
+    array.append(element)
+
+# print the array
+print("The array is:")
+# loop to print the array elements
+for i in range(size):
+    # print the array element
+    print(array[i], end=" ")
